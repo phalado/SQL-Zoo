6942d1a0d81938cede719c5d8ff83ae42279aa87
Phalado <phalado@gmail.com>
2019-11-26 19:32:54 -0300

    Done with number 6

--- 5-SUM-and-COUNT.sql
+++ 5-SUM-and-COUNT.sql
@@ -22,12 +22,19 @@ SELECT SUM(population)
   FROM world
   WHERE name IN ('Estonia', 'Latvia', 'Lithuania');
 
---
+-- For each continent show the continent and number of countries.
+SELECT continent, COUNT(name)
+  FROM world
+  GROUP BY continent;
 
+-- For each continent show the continent and number of countries with populations of at least 10 million.
+SELECT continent, COUNT(name)
+  FROM world
+  WHERE population >= 10000000
+  GROUP BY continent;
 
---
-
-
---
-
-
+-- List the continents that have a total population of at least 100 million.
+SELECT continent
+  FROM world
+  GROUP BY continent
+  HAVING SUM(population) >= 100000000;
